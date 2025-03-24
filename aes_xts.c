182562f54f6d9bd169ce18ca14b48f9770a94ecd
XomByik <kamber002@outlook.sk>
2025-03-24 22:29:08 +0100

    Mon Mar 24 22:29:08 CET 2025

--- aes_xts.c
+++ aes_xts.c
@@ -44,22 +44,34 @@ int32_t aes_xts_crypt_sector(
     int encrypt,
     int key_bits 
 ) {
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    if (!ctx) {
-        print_openssl_error();
-        return AES_XTS_ERROR_OPENSSL;
-    }
-
+    EVP_CIPHER_CTX *ctx;
     uint8_t iv[IV_SIZE] = {0};
-    *(uint64_t*)iv = sector_num + (encrypt ? RESERVED_SECTORS : 0);
-
+    int len;
     size_t key_size = key_bits / BITS_PER_BYTE;
-    uint8_t combined_key[KEY_SIZE * 2];
+    uint8_t combined_key[KEY_SIZE * 2]; 
+    const EVP_CIPHER *cipher;
+    
+    #ifdef _WIN32
+    uint64_t effective_sector = sector_num + RESERVED_SECTORS;
+    #else
+    uint64_t effective_sector = sector_num;
+    #endif
+    
+    *(uint64_t*)iv = effective_sector;
+    
     memcpy(combined_key, key1, key_size);
     memcpy(combined_key + key_size, key2, key_size);
 
-    const EVP_CIPHER *cipher = (key_bits == 128) ? EVP_aes_128_xts() : EVP_aes_256_xts();
-    int len;
+    if (key_bits == 128) {
+        cipher = EVP_aes_128_xts();
+    } else {
+        cipher = EVP_aes_256_xts(); 
+    }
+    
+    if (!(ctx = EVP_CIPHER_CTX_new())) {
+        print_openssl_error();
+        return AES_XTS_ERROR_OPENSSL;
+    }
 
     if (encrypt) {
         if (EVP_EncryptInit_ex(ctx, cipher, NULL, combined_key, iv) != 1 ||
@@ -95,89 +107,107 @@ int derive_keys_from_password(
 ) {
     size_t key_len = key_bits / BITS_PER_BYTE;
     uint8_t combined_key[AES_KEY_LENGTH_256 * 2];
-
+    
     EVP_KDF *kdf = EVP_KDF_fetch(NULL, "ARGON2ID", NULL);
     if (!kdf) {
         fprintf(stderr, "Chyba: Argon2id nie je dostupny v tejto verzii OpenSSL\n");
         print_openssl_error();
         return AES_XTS_ERROR_OPENSSL;
     }
-
+    
     EVP_KDF_CTX *kctx = EVP_KDF_CTX_new(kdf);
     EVP_KDF_free(kdf);
+    
     if (!kctx) {
         print_openssl_error();
         return AES_XTS_ERROR_OPENSSL;
     }
-
-    OSSL_PARAM params[] = {
-        OSSL_PARAM_construct_octet_string("pass", (void*)password, strlen((const char*)password)),
-        OSSL_PARAM_construct_octet_string("salt", (void*)salt, salt_len),
-        OSSL_PARAM_construct_uint32("iterations", &iterations),
-        OSSL_PARAM_construct_uint32("m_cost", &memory_cost),
-        OSSL_PARAM_construct_uint32("parallelism", &(uint32_t){DEFAULT_PARALLELISM}),
-        OSSL_PARAM_construct_end()
-    };
-
+    
+    OSSL_PARAM params[6];
+    params[0] = OSSL_PARAM_construct_octet_string("pass", (void*)password, strlen((const char*)password));
+    params[1] = OSSL_PARAM_construct_octet_string("salt", (void*)salt, salt_len);
+    params[2] = OSSL_PARAM_construct_uint32("iterations", &iterations);
+    params[3] = OSSL_PARAM_construct_uint32("m_cost", &memory_cost);
+    params[4] = OSSL_PARAM_construct_uint32("parallelism", &(uint32_t){DEFAULT_PARALLELISM}); 
+    params[5] = OSSL_PARAM_construct_end();
+    
     if (EVP_KDF_derive(kctx, combined_key, key_len * 2, params) <= 0) {
         print_openssl_error();
         EVP_KDF_CTX_free(kctx);
         return AES_XTS_ERROR_OPENSSL;
     }
-
+    
     EVP_KDF_CTX_free(kctx);
+    
     memcpy(key1, combined_key, key_len);
     memcpy(key2, combined_key + key_len, key_len);
-
+    
     return AES_XTS_SUCCESS;
 }
 
 void read_password(uint8_t *password, size_t max_len, const char *prompt) {
     printf("%s", prompt);
     fflush(stdout);
-
+    
     size_t i = 0;
     int c;
-
+    
     #ifdef _WIN32
     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
     DWORD mode = 0;
     GetConsoleMode(hStdin, &mode);
     SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
-
-    while (i < max_len - 1 && (c = _getch()) != '\r' && c != '\n') {
-        if (c == '\b' && i > 0) {
+    
+    while (i < max_len - 1) {
+        c = _getch();
+        
+        if (c == '\r' || c == '\n') { 
+            break;
+        }
+        else if (c == '\b' && i > 0) {
             i--;
-            printf("\b \b");
-        } else if (c >= 32 && c <= 255) {
+            printf("\b \b"); 
+            fflush(stdout);
+        }
+        else if (c >= 32 && c <= 255) {
             password[i++] = c;
             printf("*");
+            fflush(stdout);
         }
     }
-
+    
     SetConsoleMode(hStdin, mode);
     #else
     struct termios old_term, new_term;
     tcgetattr(STDIN_FILENO, &old_term);
     new_term = old_term;
     new_term.c_lflag &= ~(ECHO);
+    
     tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
-
-    while (i < max_len - 1 && (c = getchar()) != '\n' && c != EOF) {
-        if (c == 127 || c == '\b') {
+    
+    while (i < max_len - 1) {
+        c = getchar();
+        
+        if (c == '\n' || c == EOF) {
+            break;
+        }
+        else if (c == 127 || c == '\b') { 
             if (i > 0) {
                 i--;
                 printf("\b \b");
+                fflush(stdout);
             }
-        } else if (c >= 0 && c <= 255) {
+        }
+        else if (c >= 0 && c <= 255) {
             password[i++] = c;
             printf("*");
+            fflush(stdout);
         }
     }
-
+    
     tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
     #endif
-
+    
     password[i] = '\0';
     printf("\n");
 }
@@ -229,17 +259,17 @@ void close_device(device_context_t *ctx) {
 }
 
 uint8_t* allocate_aligned_buffer(size_t size) {
+    uint8_t* buffer = NULL;
     #ifdef _WIN32
-    uint8_t* buffer = (uint8_t *)_aligned_malloc(size, SECTOR_SIZE);
+    buffer = (uint8_t *)_aligned_malloc(size, SECTOR_SIZE);
+    #else
+    if (posix_memalign((void**)&buffer, SECTOR_SIZE, size) != 0) {
+        buffer = NULL;
+    }
+    #endif
     if (buffer) {
         memset(buffer, 0, size);
     }
-    #else
-    uint8_t* buffer = NULL;
-    if (posix_memalign((void**)&buffer, SECTOR_SIZE, size) == 0) {
-        memset(buffer, 0, size);
-    }
-    #endif
     return buffer;
 }
 
@@ -249,10 +279,7 @@ void secure_free_buffer(uint8_t* buffer, size_t size) {
         SecureZeroMemory(buffer, size);
         _aligned_free(buffer);
         #else
-        volatile uint8_t *p = buffer;
-        for (size_t i = 0; i < size; i++) {
-            p[i] = 0;
-        }
+        memset(buffer, 0, size);
         free(buffer);
         #endif
     }
